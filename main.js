b5715dd802112e1db0bcec3f1d14c50761e71501
inoriega <john.doe@company.com>
2020-08-25 19:08:43 -0300

    region on news2

--- main.js
+++ main.js
@@ -3,22 +3,26 @@ const news = [
     {
         title: "Brazilian President Jair Bolsonaro threatens to punch reporter in the face",
         description: "Bolsonaro, who was visiting the Metropolitan Cathedral in Brasilia on Sunday when a group of journalists met him outside, was seen on video threatening the O Globo newspaper reporter after being questioned about the allegations.",
-        src: "/assets/americas.png"
+        src: "/assets/americas.png",
+        region: "Americas"
     },
     {
         title: "Facebook prepares legal action against Thai government’s order to block group",
         description: "Facebook is planning legal action after the Thai government forced it to block a group deemed critical of the country’s monarchy.",
-        src: "/assets/asia.png"
+        src: "/assets/asia.png",
+        region: "Asia"
     },
     {
         title: "The US and China say they’re making progress on trade, even as other tensions worsen",
         description: "The United States and China say they’re optimistic about the state of their trade agreement — even as the two superpowers fight over a slew of other issues, including technology and national security.",
-        src: "/assets/china.png"
+        src: "/assets/china.png",
+        region: "China"
     },
     {
         title: "16 years old and stuck in solitary confinement 23 hours a day because of coronavirus",
         description: "John spent his 16th birthday the same way he’s spent every day during the UK’s Covid-19 lockdown — alone in a cell for 23 hours, with no visits, no internet and few phone calls. He is one of hundreds of children locked up in UK prisons, the forgotten casualties of Covid-19.",
-        src: "/assets/europe.png"
+        src: "/assets/europe.png",
+        region: "Europe"
     },
 ];
 const validUsers = [
